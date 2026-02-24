89c8399121358b7ad43a3aa44ea1b40a0a9d3e4b
Manuel Bormans <manuel.bormans@gmail.com>
2026-02-24 15:21:36 +0100

    Add tests and pipeline script

--- src/test/java/be/pxl/cargo/api/CargoControllerTest.java
+++ src/test/java/be/pxl/cargo/api/CargoControllerTest.java
@@ -1,9 +1,105 @@
 package be.pxl.cargo.api;
 
+import be.pxl.cargo.api.request.CreateCargoRequest;
+import be.pxl.cargo.domain.Location;
+import be.pxl.cargo.exceptions.NonUniqueCodeException;
+import be.pxl.cargo.service.CargoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CargoController.class)
 public class CargoControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
 
+    @MockitoBean
+    private CargoService cargoService;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void testAddCargoSucceeds() throws Exception {
+        CreateCargoRequest validRequest = new CreateCargoRequest("C01", 150d, Location.AIRPORT_X, Location.WAREHOUSE_A);
+        mockMvc.perform(MockMvcRequestBuilders.post("/cargos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(validRequest))
+        ).andExpect(status().isCreated());
+
+        verify(cargoService, times(1)).createCargo(any(CreateCargoRequest.class));
+    }
+
+    @Test
+    public void testAddCargoWithDuplicateCode() throws Exception {
+        doThrow(new NonUniqueCodeException()).when(cargoService).createCargo(any(CreateCargoRequest.class));
+        CreateCargoRequest validRequest = new CreateCargoRequest("C01", 150d, Location.AIRPORT_X, Location.WAREHOUSE_A);
+        mockMvc.perform(MockMvcRequestBuilders.post("/cargos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(validRequest))
+        ).andExpect(status().isBadRequest());
+
+        verify(cargoService, times(1)).createCargo(any(CreateCargoRequest.class));
+    }
+
+    @Test
+    public void testAddCargoWithMissingCode() throws Exception {
+        CreateCargoRequest missingCode = new CreateCargoRequest("", 150d, Location.AIRPORT_X, Location.WAREHOUSE_A);
+        mockMvc.perform(MockMvcRequestBuilders.post("/cargos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(missingCode))
+        ).andExpect(status().isBadRequest());
+
+            verify(cargoService, never()).createCargo(any(CreateCargoRequest.class));
+    }
+
+    @Test
+    public void testAddCargoWithInvalidWeight() throws Exception {
+        CreateCargoRequest invalidWeight = new CreateCargoRequest("C02", 50d, Location.AIRPORT_X, Location.WAREHOUSE_A);
+        mockMvc.perform(MockMvcRequestBuilders.post("/cargos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(invalidWeight))
+        ).andExpect(status().isBadRequest());
+
+        verify(cargoService, never()).createCargo(any(CreateCargoRequest.class));
+    }
+
+    @Test
+    public void testAddCargoWithInvalidCargoOrigin() throws Exception {
+        CreateCargoRequest missingOrigin = new CreateCargoRequest("C03", 150d, null, Location.WAREHOUSE_A);
+        mockMvc.perform(MockMvcRequestBuilders.post("/cargos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(missingOrigin))
+        ).andExpect(status().isBadRequest());
+
+        verify(cargoService, never()).createCargo(any(CreateCargoRequest.class));
+    }
+
+    @Test
+    public void testAddCargoWithInvalidCargoDestination() throws Exception {
+        CreateCargoRequest missingDestination = new CreateCargoRequest("C04", 150d, Location.AIRPORT_X, null);
+        mockMvc.perform(MockMvcRequestBuilders.post("/cargos")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(missingDestination))
+        ).andExpect(status().isBadRequest());
+
+        verify(cargoService, never()).createCargo(any(CreateCargoRequest.class));
+    }
+
+    @Test
+    public void testGetCargoStatistics() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/cargos/statistics"))
+                .andExpect(status().isOk());
+
+        verify(cargoService, times(1)).getCargoStatistics();
+    }
 }
--- src/test/java/be/pxl/cargo/service/CargoServiceTest.java
+++ src/test/java/be/pxl/cargo/service/CargoServiceTest.java
@@ -1,9 +1,65 @@
 package be.pxl.cargo.service;
 
+import be.pxl.cargo.api.request.CreateCargoRequest;
+import be.pxl.cargo.api.response.CargoStatistics;
+import be.pxl.cargo.domain.Cargo;
+import be.pxl.cargo.domain.Location;
+import be.pxl.cargo.exceptions.NonUniqueCodeException;
+import be.pxl.cargo.repository.CargoRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CargoServiceTest {
 
+    @Mock
+    private CargoRepository cargoRepository;
+
+    @InjectMocks
+    private CargoService cargoService;
+
+    @Test
+    public void testCreateCargoSucceeds() {
+        CreateCargoRequest validRequest = new CreateCargoRequest("C01", 150d, Location.AIRPORT_X, Location.WAREHOUSE_A);
+        cargoService.createCargo(validRequest);
+        verify(cargoRepository, times(1)).save(any(Cargo.class));
+    }
+
+    @Test
+    public void testCreateCargoFailsWithDuplicateCode() {
+        when(cargoRepository.findCargoByCode(any())).thenReturn(Optional.of(new Cargo()));
+        CreateCargoRequest validRequest = new CreateCargoRequest("C01", 150d, Location.AIRPORT_X, Location.WAREHOUSE_A);
+        assertThrows(NonUniqueCodeException.class, () -> cargoService.createCargo(validRequest));
+        verify(cargoRepository, never()).save(any(Cargo.class));
+    }
+
+    @Test
+    public void testGetCargoStatistics() {
+        Cargo cargo = new Cargo("CARGO1", 200d, Location.AIRPORT_X, Location.WAREHOUSE_A);
+        Cargo cargo2 = new Cargo("CARGO2", 300d, Location.AIRPORT_X, Location.WAREHOUSE_A);
+        cargo2.arrive(Location.SEA_PORT_Z);
+        Cargo cargo3 = new Cargo("CARGO3", 250d, Location.AIRPORT_X, Location.WAREHOUSE_A);
+        cargo3.arrive(Location.WAREHOUSE_A);
+        when(cargoRepository.findAll()).thenReturn(List.of(cargo, cargo2, cargo3));
+
+        CargoStatistics statistics = cargoService.getCargoStatistics();
+        assertEquals(3, statistics.getStatusCount().size());
+        assertEquals(cargo2.getCode(), statistics.getHeaviestCargo());
+        assertEquals(250d, statistics.getAverageCargoWeight());
+        assertEquals(1, statistics.getCountCargosAtWarehouseA());
+        assertEquals(0d, statistics.getTotalWeightDeliveredAtCityB());
+
+        verify(cargoRepository, times(1)).findAll();
+    }
 }
